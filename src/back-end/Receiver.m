9dfcf227250046972029c159b8fa7ac946a9647c
Chien-Chun Wang <james.wang0630@gmail.com>
2024-06-26 23:24:59 +0200

    Connect with GUI, bug fix

--- src/back-end/Receiver.m
+++ src/back-end/Receiver.m
@@ -8,10 +8,11 @@ classdef Receiver < handle
         function obj = Receiver()
         end
 
-        function plot3dRoom(obj, imageSource, backgroundPolygon, vanshingPoint)
+        function plot3dRoom(obj, imageSource, backgroundPolygon, vanishingPoint)
             % where does the depth come from? making arbitrary assumption
             % here we have to apply the spidery mesh
             % taking test values for now TODO: implement this!
+            points = get12Points(imageSource, vanishingPoint, backgroundPolygon);
 
             roomDepth = 500;
             roomHeight = 500;
@@ -19,22 +20,22 @@ classdef Receiver < handle
             
             
             % manually assign Points 
-            points = zeros(12, 2);
-            
-            points(1, :) = [290, 412];
-            points(2, :) = [570, 412];
-            points(3, :) = [1, 620];
-            points(4, :) = [848, 620];
-            points(5, :) = [1, 620];
-            points(6, :) = [848, 620];
-            points(7, :) = [290, 206];
-            points(8, :) = [570, 206];
-            points(9, :) = [0, 0];
-            points(10, :) = [848, 0];
-            points(11, :) = [0, 0];
-            points(12, :) = [848, 0];
+            % points = zeros(12, 2);
+            % 
+            % points(1, :) = [290, 412];
+            % points(2, :) = [570, 412];
+            % points(3, :) = [1, 620];
+            % points(4, :) = [848, 620];
+            % points(5, :) = [1, 620];
+            % points(6, :) = [848, 620];
+            % points(7, :) = [290, 206];
+            % points(8, :) = [570, 206];
+            % points(9, :) = [0, 0];
+            % points(10, :) = [848, 0];
+            % points(11, :) = [0, 0];
+            % points(12, :) = [848, 0];
 
-            threeDplot_function(imageSource.CData, points, roomDepth, roomHeight, roomWidth)
+            threeDplot_function(imageSource.CData, [points(:, 2), points(:, 1)], roomDepth, roomHeight, roomWidth)
         end
     end
 end
