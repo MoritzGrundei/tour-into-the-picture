bf76b568b8f8f3a60f7d098e701b26f6e300e963
Thomas Schwarzfischer <thomas.schwarzfischer@outlook.com>
2024-06-28 20:31:23 +0200

    Add function to plot foreground objects

---
+++ src/back-end/plot_foreground_object.m
@@ -0,0 +1,11 @@
+function plot_foreground_object(texture, x, y, z, width, height)
+% PLOT_FOREGROUND_OBJECT plots a 2D surface on a specified position in the
+% 3D plot.
+% x, y, z: lower left corner coordinates of the rectangle
+
+corners_x = [x x+width; x x+width];
+corners_y = [y y; y y];
+corners_z = [z+height z+height; z z];
+surface(corners_x, corners_y, corners_z, 'FaceColor', 'texturemap', 'CData', texture);
+
+end
--- test/threeDplot_test.m
+++ test/threeDplot_test.m
@@ -5,7 +5,7 @@
 
 % assign 2D parameters, later calculated  
 roomDepth = 500;
-roomHeight = 500;
+roomHeight = 300;
 roomWidth = 500;
 
 
@@ -29,4 +29,18 @@ Points(12, :) = [848, 0];
 input_image = imread('static/images/simple-room.png');
 
 % function that generates 3D plot with given dummy data
-threeDplot_function(input_image,Points, roomDepth, roomHeight, roomWidth); 
+threeDplot_function(input_image,Points, roomDepth, roomHeight, roomWidth);
+
+% add a foreground object
+fg_points = [
+    350,460; %TR
+    495,460; %TL
+    300,590; %BR
+    540,590; %BL
+    ];
+
+% transformation usually not necessary
+foreground_object = projective_transformation(input_image, fg_points(1, :), fg_points(2, :), fg_points(3, :), fg_points(4, :), 150, 100);
+
+% add to plot
+plot_foreground_object(foreground_object, 150, 50, 0, 200, 100);
