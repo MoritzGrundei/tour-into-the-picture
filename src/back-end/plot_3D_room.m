f1cfce7db842a7c2eadf8168ac7ec5e6db5711b0
Martin Ausborn <martin.ausborn@tum.de>
2024-07-04 17:18:45 +0200

    Updated plot_3D_room to fit new room variable assignments

--- src/back-end/plot_3D_room.m
+++ src/back-end/plot_3D_room.m
@@ -1,53 +1,53 @@
-function plot_3D_room(leftWall, rightWall, rearWall, floorWall, ceilingWall)
+function plot_3D_room(walls)
 %% PLOT_3D:ROOM: transform a 2D image into a 3D plot
 % 2D images of the 5 room walls
 
 % extract room dimensions from input arguments
-roomWidth = size(rearWall, 2);
-roomHeight = size(rearWall, 1);
-leftDepth = size(leftWall, 2);
-rightDepth = size(rightWall, 2);
-floorDepth = size(floorWall, 1);
-ceilingDepth = size(ceilingWall, 1);
+roomWidth = size(walls{5}, 2);
+roomHeight = size(walls{5}, 1);
+leftDepth = size(walls{2}, 2);
+rightDepth = size(walls{3}, 2);
+floorDepth = size(walls{1}, 1);
+ceilingDepth = size(walls{4}, 1);
 
 average_depth = (leftDepth + rightDepth + ceilingDepth + floorDepth) / 4;
 
 % Create figure
 f = figure;
 
+% Plot floor
+floorX = [0 roomWidth; 0 roomWidth];
+floorY = [0 0; 0 0];
+floorZ = [0 0; floorDepth floorDepth];
+h = surface(floorX, floorY, floorZ, 'FaceColor', 'texturemap', 'CData', walls{1});
+set(h, 'EdgeColor', 'none');
+
 % Plot left wall
 leftX = [0 0; 0 0];
 leftY = [roomHeight roomHeight; 0 0];
 leftZ = [leftDepth 0; leftDepth 0];
-h = surface(leftX, leftY, leftZ, 'FaceColor', 'texturemap', 'CData', leftWall);
+h = surface(leftX, leftY, leftZ, 'FaceColor', 'texturemap', 'CData', walls{2});
 set(h, 'EdgeColor', 'none');
 
 % Plot right wall
 rightX = [roomWidth roomWidth; roomWidth roomWidth];
 rightY = [roomHeight roomHeight; 0 0];
 rightZ = [0 rightDepth; 0 rightDepth];
-h = surface(rightX, rightY, rightZ, 'FaceColor', 'texturemap', 'CData', rightWall);
-set(h, 'EdgeColor', 'none');
-
-% Plot rear wall
-rearX = [0 roomWidth; 0 roomWidth];
-rearY = [roomHeight roomHeight; 0 0];
-rearZ = [0 0; 0 0];
-h = surface(rearX, rearY, rearZ, 'FaceColor', 'texturemap', 'CData', rearWall);
-set(h, 'EdgeColor', 'none');
-
-% Plot floor
-floorX = [0 roomWidth; 0 roomWidth];
-floorY = [0 0; 0 0];
-floorZ = [0 0; floorDepth floorDepth];
-h = surface(floorX, floorY, floorZ, 'FaceColor', 'texturemap', 'CData', floorWall);
+h = surface(rightX, rightY, rightZ, 'FaceColor', 'texturemap', 'CData', walls{3});
 set(h, 'EdgeColor', 'none');
 
 % Plot ceiling
 ceilingX = [0 roomWidth; 0 roomWidth];
 ceilingY = [roomHeight roomHeight; roomHeight roomHeight];
 ceilingZ = [ceilingDepth ceilingDepth; 0 0];
-h = surface(ceilingX, ceilingY, ceilingZ, 'FaceColor', 'texturemap', 'CData', ceilingWall);
+h = surface(ceilingX, ceilingY, ceilingZ, 'FaceColor', 'texturemap', 'CData', walls{4});
+set(h, 'EdgeColor', 'none');
+
+% Plot rear wall
+rearX = [0 roomWidth; 0 roomWidth];
+rearY = [roomHeight roomHeight; 0 0];
+rearZ = [0 0; 0 0];
+h = surface(rearX, rearY, rearZ, 'FaceColor', 'texturemap', 'CData', walls{5});
 set(h, 'EdgeColor', 'none');
 
 % Set up the view
--- src/back-end/plot_foreground_object.m
+++ src/back-end/plot_foreground_object.m
@@ -1,12 +1,27 @@
-function plot_foreground_object(texture, x, y, z, width, height)
+function plot_foreground_object(Points, polygon, rectangle, imageSource, walls, )
 % PLOT_FOREGROUND_OBJECT plots a 2D surface on a specified position in the
 % 3D plot.
 % x, y, z: lower left corner coordinates of the rectangle
 
+% function to determine side of foreground object
+wall_number = get_wall_number(Points, rectangle);
+
+% function to get 3D coordinates of foreground
+[TL, TR, BR, BL] = foreground_coordinates(tform{wall}, rectangle, wall_number, walls); 
+
+% scaling 
+
+corners_x = [TL(1) TR(1); BL(1) BR(2)];
+corners_y = [y y; y y];
+corners_z = [z+height z+height; z z];
+h = surface(corners_x, corners_y, corners_z, 'FaceColor', 'texturemap', 'CData', polygon);
+set(h, 'EdgeColor', 'none');
+
+
 corners_x = [x x+width; x x+width];
 corners_y = [y y; y y];
 corners_z = [z+height z+height; z z];
-h = surface(corners_x, corners_y, corners_z, 'FaceColor', 'texturemap', 'CData', texture);
+h = surface(corners_x, corners_y, corners_z, 'FaceColor', 'texturemap', 'CData', polygon);
 set(h, 'EdgeColor', 'none');
 
 end
