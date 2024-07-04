8078a6e5d2b2d50ebd19ee0cd1dd588a7ee544bd
Andi <andi.umlauf@t.online.de>
2024-07-04 18:34:07 +0200

    implementation of the 3D reconstruction and test as a reference

--- src/back-end/Receiver.m
+++ src/back-end/Receiver.m
@@ -12,7 +12,11 @@ classdef Receiver < handle
             % where does the depth come from? making arbitrary assumption
 
             points = get12Points(imageSource.CData, vanishingPoint, backgroundPolygon);
-            
+            threeD_points = get_threeD_points(points,vanishingPoint);
+            testpoints = test(vanishingPoint,points')';
+            %roomDepth =500;
+            %roomWidth = 500;
+            %roomHeight = 500;
             [roomDepth, roomHeight, roomWidth] = get_room_dimensions(backgroundPolygon,imageSource.CData);
 
             threeDplot_function(imageSource.CData, [points(:, 2), points(:, 1)], roomDepth, roomHeight, roomWidth)
