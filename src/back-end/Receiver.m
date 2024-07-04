16f711d9e5222775218200bd2ae985813824b5e1
Martin Ausborn <martin.ausborn@tum.de>
2024-07-04 16:58:01 +0200

    Calculate 3D coordinates of foreground object

--- src/back-end/Receiver.m
+++ src/back-end/Receiver.m
@@ -24,16 +24,27 @@ classdef Receiver < handle
             Points = [points(:, 2), points(:, 1)];
 
             % calculate each wall perspective
-            leftWall = projective_transformation(imageSource.CData,Points(11, :),Points(7, :),Points(5, :),Points(1, :),roomDepth,roomHeight);
-            rightWall = projective_transformation(imageSource.CData,Points(8, :),Points(12, :),Points(2, :),Points(6, :),roomDepth,roomHeight);
-            rearWall = projective_transformation(imageSource.CData,Points(7, :),Points(8, :),Points(1, :),Points(2, :),roomWidth,roomHeight);
-            floorWall = projective_transformation(imageSource.CData,Points(1, :),Points(2, :),Points(3, :),Points(4, :),roomWidth,roomDepth);
-            ceilingWall = projective_transformation(imageSource.CData,Points(9, :),Points(10, :),Points(7, :),Points(8, :),roomWidth,roomDepth);
+            % init array 
+            tform = cell(5);
+            walls = cell(5);
+            % floor cell{1}
+            % left wall  cell{2}
+            % right wall cell{3}
+            % ceiling cell{4}
+            % rear wall cell{5}
+
+            [walls{1}, tform{1}] = projective_transformation(imageSource.CData,Points(1, :),Points(2, :),Points(3, :),Points(4, :),roomWidth,roomDepth);
+            [walls{2}, tform{2}] = projective_transformation(imageSource.CData,Points(11, :),Points(7, :),Points(5, :),Points(1, :),roomDepth,roomHeight);
+            [walls{3}, tform{3}] = projective_transformation(imageSource.CData,Points(8, :),Points(12, :),Points(2, :),Points(6, :),roomDepth,roomHeight);
+            [walls{4}, tform{4}] = projective_transformation(imageSource.CData,Points(9, :),Points(10, :),Points(7, :),Points(8, :),roomWidth,roomDepth);
+            [walls{5}, tform{5}] = projective_transformation(imageSource.CData,Points(7, :),Points(8, :),Points(1, :),Points(2, :),roomWidth,roomHeight);
             
             % construct room
-            plot_3D_room(leftWall,rightWall,rearWall,floorWall,ceilingWall);
+            plot_3D_room(walls);
 
             % add forreground objects
+            plot_foreground_object(Points, polygon, rectangle, imageSource.CData, walls, tform); 
+            
         end
     end
 end
