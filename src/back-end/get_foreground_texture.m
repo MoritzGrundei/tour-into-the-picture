fe7e1e7b5d4a94a1b77d390989e1597fb8cda7ea
Thomas Schwarzfischer <thomas.schwarzfischer@outlook.com>
2024-07-05 14:24:15 +0200

    Add function get_foreground_texture.m

---
+++ src/back-end/get_foreground_texture.m
@@ -0,0 +1,19 @@
+function texture = get_foreground_texture(whole_image, mask, frame)
+%GET_FOREGROUND_TEXTURE returns the texture of a foreground image
+
+% Image dimensions
+width = frame(2,1) - frame(1,1) + 1;
+height = frame(3,2) - frame(2,2) + 1;
+
+% Create a new image
+texture = uint8(zeros(height, width, 3));
+
+% Apply the mask to keep only the object in the new image
+for channel = 1:3
+    masked_channel = whole_image(:,:,channel) .* uint8(mask);
+    texture(:,:,channel) = masked_channel(frame(1,2):frame(4,2),frame(1,1):frame(2,1));
+end
+
+figure;
+imshow(texture);
+end
