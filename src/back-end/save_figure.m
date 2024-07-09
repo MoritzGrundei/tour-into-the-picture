32de2625c4b0dc6d371b79b344da66dd4faa9aa0
martinausborn <martin.ausborn@tum.de>
2024-07-09 18:39:26 +0200

    Save figure in output folder: working on all systems

--- src/back-end/save_figure.m
+++ src/back-end/save_figure.m
@@ -1,15 +1,17 @@
 function save_figure(hfig, imageFileName)
-%SAVE_FIGURE saves the 3d room as a .fig file; create the output folder if
-% necessary
+% SAVE_FIGURE saves the 3D room as a .fig file; create the output folder if necessary
 
 % Extract image name from file path
 [~, imageName, ~] = fileparts(imageFileName);
 
-mfolder = fileparts(which(mfilename));
-% Split the path into its components
-pathComponents = split(mfolder, filesep);
+% Get the directory of the main.m script
+mainFilePath = which('main');
+if isempty(mainFilePath)
+    error('Cannot find main.m file.');
+end
+mainFolder = fileparts(mainFilePath);
 
-mainFolder = fullfile(pathComponents{1:length(pathComponents)-2});
+% Define the output folder name
 outputFolder = 'output';
 
 % Construct the directory path using fullfile
@@ -20,15 +22,16 @@ if ~exist(outputDir, 'dir')
     mkdir(outputDir);
 end
 
-% Check for existing files and update the filename accordingly
+% Initialize the counter for file name uniqueness
 counter = 1;
-%saveFilename = ['/', mainFolder, '/output/', imageName, '_3Dplot_', sprintf('%03d', counter), '.fig'];
-saveFilename = fullfile(outputDir, [imageName, '_3Dplot_', sprintf('%03d', counter), '.fig']);
 
+% Construct the initial file path using fullfile
+saveFilename = fullfile(outputDir, [imageName, '3Dplot', sprintf('%03d', counter), '.fig']);
+
+% Loop to ensure the filename is unique
 while exist(saveFilename, 'file')
-    % Update the filename with the counter
-    saveFilename = fullfile(outputDir, [imageName, '_3Dplot_', sprintf('%03d', counter), '.fig']);
     counter = counter + 1;
+    saveFilename = fullfile(outputDir, [imageName, '3Dplot', sprintf('%03d', counter), '.fig']);
 end
 
 % Save the figure to a .fig file
