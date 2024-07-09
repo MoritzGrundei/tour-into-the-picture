e8548ce27c3b32043d597587162d043182712219
Thomas Schwarzfischer <thomas.schwarzfischer@outlook.com>
2024-07-09 14:43:59 +0200

    Add figure saving compatibility for all operating systems

--- src/back-end/save_figure.m
+++ src/back-end/save_figure.m
@@ -10,18 +10,24 @@ mfolder = fileparts(which(mfilename));
 pathComponents = split(mfolder, filesep);
 
 mainFolder = fullfile(pathComponents{1:length(pathComponents)-2});
+outputFolder = 'output';
 
-% Create 'output' directory if it doesn't exist
-if ~exist(['/', mainFolder, '/output'], 'dir')
-    mkdir(['/', mainFolder, '/output']);
+% Construct the directory path using fullfile
+outputDir = fullfile(mainFolder, outputFolder);
+
+% Check if the directory exists, and create it if it does not
+if ~exist(outputDir, 'dir')
+    mkdir(outputDir);
 end
 
 % Check for existing files and update the filename accordingly
 counter = 1;
-saveFilename = ['/', mainFolder, '/output/', imageName, '_3Dplot_', sprintf('%03d', counter), '.fig'];
+%saveFilename = ['/', mainFolder, '/output/', imageName, '_3Dplot_', sprintf('%03d', counter), '.fig'];
+saveFilename = fullfile(outputDir, [imageName, '_3Dplot_', sprintf('%03d', counter), '.fig']);
+
 while exist(saveFilename, 'file')
     % Update the filename with the counter
-    saveFilename = [mainFolder, '/output/', imageName, '_3Dplot_', sprintf('%03d', counter), '.fig'];
+    saveFilename = fullfile(outputDir, [imageName, '_3Dplot_', sprintf('%03d', counter), '.fig']);
     counter = counter + 1;
 end
 
